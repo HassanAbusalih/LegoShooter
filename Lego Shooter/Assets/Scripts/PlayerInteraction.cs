86e5d7ce594986efb8ab58318090b3b960f86478
HassanAbusalih <hassan.abusalih@student.saedubai.com>
2023-03-14 20:18:44 +0400

    Added player interaction and movement, edited spawner

--- Lego Shooter/Assets/Scripts/PlayerInteraction.cs
+++ Lego Shooter/Assets/Scripts/PlayerInteraction.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    //Life is pain.
+    [SerializeField] float pickUpDistance = 2f;
+    [SerializeField] string gunTag;
+    [SerializeField] Transform carryPos;
+    Transform currentObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, pickUpDistance))
+        {
+            if (hit.transform.tag == gunTag)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    currentObject = Instantiate(hit.transform);
+                    currentObject.parent = carryPos;
+                    currentObject.localPosition = Vector3.zero;
+                }
+            }
+        }
+        if (currentObject != null && Input.GetKeyDown(KeyCode.E))
+        {
+            currentObject.parent = null;
+            currentObject = null;
+        }
     }
 }
